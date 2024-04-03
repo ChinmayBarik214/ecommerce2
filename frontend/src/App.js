5ea8ac8de5f44e3d44b91c033bc3ce63651923bf
Chinmay <chinmaybarik214@gmail.com>
2024-04-03 11:02:59 +0530

    got it working on js files as well!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
-import "tailwindcss/tailwind.css"
 function App() {
   return (
     <div className="App">
