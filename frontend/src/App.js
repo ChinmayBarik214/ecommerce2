dd6e14da09c6ad507f3d342d03ec1f13310517ef
Chinmay <chinmaybarik214@gmail.com>
2024-04-03 13:33:06 +0530

    tut: 19:11 added product list tailwind component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import { Counter } from "./features/counter/Counter";
 import "tailwindcss/tailwind.css";
+import ProductList from "./features/product-list/ProductList";
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      <ProductList />
     </div>
   );
 }
