cfd6623a4a30fa0057dd64e147881aea5ad837b1
Chinmay <chinmaybarik214@gmail.com>
2024-04-05 01:27:30 +0530

    tut 1:23:43 before ProductDetail page

--- frontend/src/features/product/components/ProductList.js
+++ frontend/src/features/product/components/ProductList.js
@@ -4,7 +4,11 @@ import { increment, incrementAsync, selectCount } from "../productSlice";
 import { Link } from "react-router-dom";
 import { Dialog, Disclosure, Menu, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  StarIcon,
+} from "@heroicons/react/20/solid";
 import {
   ChevronDownIcon,
   FunnelIcon,
@@ -910,8 +914,11 @@ export default function ProductList() {
                         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                           {products.map((product) => (
                             <Link to="/product-detail">
-                              <div key={product.id} className="group relative">
-                                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                              <div
+                                key={product.id}
+                                className="group relative border-solid border-2 p-2 border-gray-200"
+                              >
+                                <div className="min-h-60 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-60">
                                   <img
                                     src={product.thumbnail}
                                     alt={product.title}
@@ -930,12 +937,24 @@ export default function ProductList() {
                                       </a>
                                     </h3>
                                     <p className="mt-1 text-sm text-gray-500">
-                                      {product.color}
+                                      <StarIcon className="w-6 h-6 inline" />
+                                      <span className="align-bottom">
+                                        {product.rating}
+                                      </span>
                                     </p>
                                   </div>
-                                  <p className="text-sm font-medium text-gray-900">
-                                    {product.price}
-                                  </p>
+                                  <div>
+                                    <p className="text-sm block font-medium text-gray-900">
+                                      ₹
+                                      {Math.round(
+                                        product.price *
+                                          (1 - product.discountPercentage / 100)
+                                      )}
+                                    </p>
+                                    <p className="text-sm block line-through font-medium text-gray-400">
+                                      ₹{product.price}
+                                    </p>
+                                  </div>
                                 </div>
                               </div>
                             </Link>
