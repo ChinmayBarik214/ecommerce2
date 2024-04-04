685c803c36ba2458eb887c9799b86f8be784c61e
Chinmay <chinmaybarik214@gmail.com>
2024-04-04 13:13:36 +0530

    tut 1:00:50 before find and replace

--- frontend/src/pages/Checkout.js
+++ frontend/src/pages/Checkout.js
@@ -206,82 +206,10 @@ function Checkout() {
                 <div className="mt-10 space-y-10">
                   <fieldset>
                     <legend className="text-sm font-semibold leading-6 text-gray-900">
-                      By Email
-                    </legend>
-                    <div className="mt-6 space-y-6">
-                      <div className="relative flex gap-x-3">
-                        <div className="flex h-6 items-center">
-                          <input
-                            id="comments"
-                            name="comments"
-                            type="checkbox"
-                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                          />
-                        </div>
-                        <div className="text-sm leading-6">
-                          <label
-                            htmlFor="comments"
-                            className="font-medium text-gray-900"
-                          >
-                            Comments
-                          </label>
-                          <p className="text-gray-500">
-                            Get notified when someones posts a comment on a
-                            posting.
-                          </p>
-                        </div>
-                      </div>
-                      <div className="relative flex gap-x-3">
-                        <div className="flex h-6 items-center">
-                          <input
-                            id="candidates"
-                            name="candidates"
-                            type="checkbox"
-                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                          />
-                        </div>
-                        <div className="text-sm leading-6">
-                          <label
-                            htmlFor="candidates"
-                            className="font-medium text-gray-900"
-                          >
-                            Candidates
-                          </label>
-                          <p className="text-gray-500">
-                            Get notified when a candidate applies for a job.
-                          </p>
-                        </div>
-                      </div>
-                      <div className="relative flex gap-x-3">
-                        <div className="flex h-6 items-center">
-                          <input
-                            id="offers"
-                            name="offers"
-                            type="checkbox"
-                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                          />
-                        </div>
-                        <div className="text-sm leading-6">
-                          <label
-                            htmlFor="offers"
-                            className="font-medium text-gray-900"
-                          >
-                            Offers
-                          </label>
-                          <p className="text-gray-500">
-                            Get notified when a candidate accepts or rejects an
-                            offer.
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </fieldset>
-                  <fieldset>
-                    <legend className="text-sm font-semibold leading-6 text-gray-900">
-                      Push Notifications
+                      Payment Methods
                     </legend>
                     <p className="mt-1 text-sm leading-6 text-gray-600">
-                      These are delivered via SMS to your mobile phone.
+                      Choose one.
                     </p>
                     <div className="mt-6 space-y-6">
                       <div className="flex items-center gap-x-3">
@@ -295,7 +223,7 @@ function Checkout() {
                           htmlFor="push-everything"
                           className="block text-sm font-medium leading-6 text-gray-900"
                         >
-                          Everything
+                          Cash
                         </label>
                       </div>
                       <div className="flex items-center gap-x-3">
@@ -309,7 +237,7 @@ function Checkout() {
                           htmlFor="push-email"
                           className="block text-sm font-medium leading-6 text-gray-900"
                         >
-                          Same as email
+                          Card Payment
                         </label>
                       </div>
                       <div className="flex items-center gap-x-3">
