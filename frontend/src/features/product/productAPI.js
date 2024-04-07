399827720ad7ba877d1b83c4c4ff07395c11efa5
Chinmay <chinmaybarik214@gmail.com>
2024-04-07 09:06:56 +0530

    revert

--- frontend/src/features/product/productAPI.js
+++ frontend/src/features/product/productAPI.js
@@ -12,25 +12,44 @@ export function fetchProductsByFilters(filter, sort, pagination) {
   // sort = {_sort: "price", _order="desc"}
   // pagination = {_page: 1, _limit=10}
   // TODO: on server we will support multiple values
-  let queryString = ""
+  let queryString = "";
   for (let key in filter) {
-    const categoryValues = filter[key]
+    const categoryValues = filter[key];
     if (categoryValues.length > 0) {
-      const lastCategoryValue = categoryValues[categoryValues.length - 1]
+      const lastCategoryValue = categoryValues[categoryValues.length - 1];
     }
-    queryString += `${key}=${filter[key]}&`
+    queryString += `${key}=${filter[key]}&`;
   }
   for (let key in sort) {
-    queryString += `${key}=${sort[key]}&`
+    queryString += `${key}=${sort[key]}&`;
   }
   for (let key in pagination) {
-    queryString += `${key}=${pagination[key]}&`
+    queryString += `${key}=${pagination[key]}&`;
   }
   return new Promise(async (resolve) => {
     // TODO: We will not hard-code server URL here
-    const response = await fetch("http://localhost:8080/products?"+queryString);
+    const response = await fetch(
+      "http://localhost:8080/products?" + queryString
+    );
     const data = await response.json();
-    const totalItems = await response.headers.get('X-Total-Count')
-    resolve({ data:{products: data, totalItems: +totalItems } });
+    const totalItems = await response.headers.get("X-Total-Count");
+    resolve({ data: { products: data, totalItems: +totalItems } });
+  });
+}
+
+
+export function fetchCategories() {
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/categories");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function fetchBrands() {
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/bands");
+    const data = await response.json();
+    resolve({ data });
   });
 }
