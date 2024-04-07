21ab60eaaa014c8ba7e89ea2361118d6ab0b1dbc
Chinmay <chinmaybarik214@gmail.com>
2024-04-07 10:32:44 +0530

    tut 2:50:44 caused param linking

--- frontend/src/features/product/productAPI.js
+++ frontend/src/features/product/productAPI.js
@@ -7,6 +7,15 @@ export function fetchAllProducts() {
   });
 }
 
+export function fetchProductsById(id) {
+  return new Promise(async (resolve) => {
+    // TODO: We will not hard-code server URL here
+    const response = await fetch("http://localhost:8080/products/"+id);
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
 export function fetchProductsByFilters(filter, sort, pagination) {
   // filter = { "category": ["smartphone", "laptops"] }
   // sort = {_sort: "price", _order="desc"}
--- frontend/src/features/product/productSlice.js
+++ frontend/src/features/product/productSlice.js
@@ -1,18 +1,28 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { fetchAllProducts, fetchProductsByFilters, fetchBrands, fetchCategories } from "./productAPI";
+import { fetchAllProducts, fetchProductsByFilters, fetchBrands, fetchCategories, fetchProductsById } from "./productAPI";
 
 const initialState = {
   products: [],
   brands: [],
   categories: [],
   status: "idle",
-  totalItems: 0
+  totalItems: 0,
+  selectedProduct: null
 };
 
 export const fetchAllProductsAsync = createAsyncThunk(
   "product/fetchAllProducts",
   async () => {
     const response = await fetchAllProducts();
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+
+export const fetchAllProductsByIdAsync = createAsyncThunk(
+  "product/fetchProductsById",
+  async () => {
+    const response = await fetchProductsById();
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
@@ -84,6 +94,13 @@ export const productSlice = createSlice({
         state.status = "idle";
         state.categories = action.payload;
       })
+      .addCase(fetchAllProductsByIdAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchAllProductsByIdAsync.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.selectedProduct = action.payload;
+      })
   },
 });
 
@@ -93,5 +110,6 @@ export const selectAllProducts = (state) => state.product.products
 export const selectTotalItems = (state) => state.product.totalItems
 export const selectBrands = (state) => state.product.brands
 export const selectCategories = (state) => state.product.categories
+export const selectedProductById = (state) => state.product.selected
 
 export default productSlice.reducer;
